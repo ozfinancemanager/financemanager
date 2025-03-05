11eb94ce4b6743047612de15be693bc8d5a438ae
Ibiella <rubyhae@gmail.com>
2025-03-05 11:52:16 +0900

    🐛 Bug fix
    .

--- config/settings/base.py
+++ config/settings/base.py
@@ -129,7 +129,3 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "models.CustomUser"
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
