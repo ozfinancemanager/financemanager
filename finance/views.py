955a4c8352156650fd9d011e20c9852b264d1dc8
2joonkim <joon271694@gmail.com>
2025-03-05 16:13:17 +0900

    🐛 bug fix

--- finance/views.py
+++ finance/views.py
@@ -3,10 +3,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import render
-from django.utils.encoding import force_bytes, force_str
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.urls import reverse
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 from config import settings
+
 from .models import CustomUser
 
 # Create your views here.
