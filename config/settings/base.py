808ac0447481012c87b916a37bcd16af130a33e8
2joonkim <joon271694@gmail.com>
2025-03-06 15:16:45 +0900

    🐛 Bug fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "djangoproject",
+        "NAME": "djangomini",
         "USER": "postgres",
         "PASSWORD": "1234",
         "HOST": "localhost",
